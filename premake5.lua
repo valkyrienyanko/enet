17059fc2088d68630febb0daa5ba798a0dcfca40
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2021-08-03 11:35:30 +0100

    Remove workspace

--- premake5.lua
+++ premake5.lua
@@ -1,51 +1,20 @@
-workspace "enet"
-	configurations { "Debug", "Release" }
-	platforms { "x32", "x64" }
-
-	project "enet_static"
-		kind "StaticLib"
-		language "C"
-		
-		files { "*.c" }
-		
-		includedirs { "include/" }
-		
-		filter "configurations:Debug"
-			targetsuffix "d"
-			defines { "DEBUG" }
-			symbols "On"
-		
-		filter "configurations:Release"
-			defines { "NDEBUG" }
-			optimize "Full"
-
-	  filter { "configurations:Debug", "platforms:x64" }
-			targetsuffix "64d"
-			
-		filter { "configurations:Release", "platforms:x64" }
-			targetsuffix "64"
-
-	project "enet"
-		kind "SharedLib"
-		language "C"
-		
-		files { "*.c" }
-		
-		includedirs { "include/" }
-		
-		defines { "ENET_DLL=1" }
-		
-		filter "configurations:Debug"
-			targetsuffix "d"
-			defines { "DEBUG" }
-			symbols "On"
-		
-		filter "configurations:Release"
-			defines { "NDEBUG" }
-			optimize "Full"
-			
-		filter { "configurations:Debug", "platforms:x64" }
-			targetsuffix "64d"
-			
-		filter { "configurations:Release", "platforms:x64" }
-			targetsuffix "64"
+project "enet"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files { "*.c", "*.h" }
+	
+	includedirs { "include/" }
+	
+	filter "configurations:Debug"
+		targetsuffix "d"
+		defines { "DEBUG" }
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "Full"
